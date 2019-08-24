026d8607d604201e5adb89bd9a051368ad0a5313
Scott Daniels <sdaniels6363@gmail.com>
2019-08-24 10:42:45 -0400

    added starting code for firebase

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCnptSb-MgdLXf1vSpIhEtP8t-YBSkU8u0",
+  authDomain: "timesheet-project-75c75.firebaseapp.com",
+  databaseURL: "https://timesheet-project-75c75.firebaseio.com",
+  projectId: "timesheet-project-75c75",
+  storageBucket: "timesheet-project-75c75.appspot.com",
+  messagingSenderId: "309321281437",
+  appId: "1:309321281437:web:06592401124cf1de"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
