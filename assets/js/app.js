5fba30a164830d59e75e8502b878a7a308bb9437
Scott Daniels <sdaniels6363@gmail.com>
2019-08-24 12:08:15 -0400

    javascript to load data from database to page

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,28 +25,58 @@ $(document).ready(function () {
     var name = $('#empName').val().trim();
     var role = $('#empRole').val().trim();
     var stDate = $('#empStartDate').val().trim();
-    var monthWrk = $('#empMonthWorked').val().trim();
+    var monthlyRate = $('#monthlyRate').val().trim();
 
     database.ref().push({
       empName: name,
       empRole: role,
       empStartDate: stDate,
-      empMonthWorked: monthWrk
+      monthlyRate: monthlyRate
     });
 
-    var emptyRow = `
-    <tr>
-      <td id="empName"></td>
-      <td id="empRole"></td>
-      <td id="empStartDate"></td>
-      <td id="empMonthWorked"></td>
-      <td id="monthlyRate"></td>
-      <td id="total"></td>
-    </tr>
-    `;
+    // Commented out because the child_added listener reloads this    
+    // var emptyRow = `
+    // <tr>
+    //   <td id="empName-display">${name}</td>
+    //   <td id="empRole-display">${role}</td>
+    //   <td id="empStartDate-display">${stDate}</td>
+    //   <td id="empMonthWorked-display"></td>
+    //   <td id="monthlyRate-display">${monthlyRate}</td>
+    //   <td id="total-display"></td>
+    // </tr>
+    // `;
 
-    $(emptyRow).appendTo("tbody");
+    // $(emptyRow).appendTo("tbody");
 
   });
+
+  // Firebase watcher .on("child_added"
+  database.ref().on("child_added", function (snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.loging the last user's data
+    console.log(sv.empName);
+    console.log(sv.empRole);
+    console.log(sv.empStartDate);
+    console.log(sv.monthlyRate);
+
+    var tableEntry = `
+    <tr>
+    <td id="empName-display">${sv.empName}</td>
+    <td id="empRole-display">${sv.empRole}</td>
+    <td id="empStartDate-display">${sv.empStartDate}</td>
+    <td id="empMonthWorked-display"></td>
+    <td id="monthlyRate-display">${sv.monthlyRate}</td>
+    <td id="total-display"></td>
+  </tr>
+    `
+    $(tableEntry).appendTo("tbody");
+
+    // Handle the errors
+  }, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
 })
 
